271765d16658ed64472087be36640b52195491a2
Daniel Szasz <daniel1.szasz@btrl.ro>
2022-11-09 18:10:25 +0200

    created the project

---
+++ TheTest/TheTest/TheTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheTestApp.swift
+//  TheTest
+//
+//  Created by BTMac on 09.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TheTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
