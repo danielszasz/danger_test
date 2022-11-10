bfedaa3b27aec19b878b8fd3b680f040fc4f8116
Daniel Szasz <daniel1.szasz@btrl.ro>
2022-11-10 18:09:18 +0200

    t

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,15 +1,5 @@
-import Danger
-import DangerXcov
+import Danger 
 let danger = Danger()
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
-
-xcov.report(
-    scheme: "TheTest",
-    workspace: "TheTest/TheTest.xcodeproj",
-    exclude_targets: "Demo.app",
-    minimum_coverage_percentage: 90
-)
-
-xcov.output_report(report)
